787693a8d2474cd114f58c8bfa1ed920078e1d36
Stanislav Orekhovskiy <stanislav.orekhovskiy@gmail.com>
2021-02-03 15:52:04 +0200

    Complete some task for dz-2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+
+  for(let i = 0; i < array.length; i++){
+    fn(array[i], i, array);
+  }
 }
+// forEach([5,8,9,20,30,55], function fn(){})
 
 /*
  Задание 2:
@@ -16,7 +21,14 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+  let copyArr = []; 
+  for(let i = 0; i < array.length; i++){
+    copyArr[i] = fn(array[i], i, array)
+    console.log(copyArr);
+  }
+  return copyArr;
 }
+// map([5,8,9,20,30,55], function fn(){})
 
 /*
  Задание 3:
@@ -25,7 +37,15 @@ function map(array, fn) {
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 function reduce(array, fn, initial) {
+  let x = initial || array[0],
+        i = initial ? 0 : 1;
+
+  for(; i < array.length; i++){
+    x = fn(x, array[i], i, array)
+  }
+  return x;
 }
+// reduce([5,8,9,20,30,55], function fn(){})
 
 /*
  Задание 4:
@@ -35,8 +55,17 @@ function reduce(array, fn, initial) {
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
+// let obj = { name: 'Сергей', lastName: 'Петров' }
 function upperProps(obj) {
+  let arr = [];
+  for (let key in obj) {
+      key = key.toUpperCase();
+      arr.push(key);
+  }
+  console.log(arr)
+  return arr;
 }
+upperProps({ name: 'Сергей', lastName: 'Петров' }); //вернет ['NAME', 'LASTNAME']
 
 /*
  Задание 5 *:
@@ -45,7 +74,19 @@ function upperProps(obj) {
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
 function slice(array, from, to) {
+
+  let newArr = [];
+
+  for(let i = 0; i < array.length; i++){
+    newArr = array[i]
+    console.log(newArr)
+    // if(array[i] < from){
+    //   console.log('hi')
+    // }
+  }
+
 }
+slice([10, 22, 34, 56, 78, 99], 0, 2)
 
 /*
  Задание 6 *:
